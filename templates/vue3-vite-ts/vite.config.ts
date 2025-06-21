8b36dbd44d83c848007c6baa0a4431561508fb47
denglian <781307@nd.com>
2025-06-21 21:24:54 +0800

    feat(template): 修复scss 异常

--- templates/vue3-vite-ts/vite.config.ts
+++ templates/vue3-vite-ts/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/styles/variables.scss";`
+        additionalData: `@use "@/styles/variables.scss" as *;`
       }
     }
   },
